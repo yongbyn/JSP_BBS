38d2f91f4d12346b4a9ac86b821f898851f4b8b8
YongBin Kim <yongbyn@naver.com>
2024-12-13 19:48:00 +0900

    게시글 보기 기능 구현

--- src/main/java/bbs/Bbs.java
+++ src/main/java/bbs/Bbs.java
@@ -6,7 +6,7 @@ public class Bbs {
 	private String bbsTitle;
 	private String userID;
 	private String bbsDate;
-	private String bbsContent;
+	private String bbsContent;	
 	private int bbsAvailable;
 	public int getBbsID() {
 		return bbsID;
